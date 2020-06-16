0fe31fee73f72604690fe79a9eb7421b93822c1e
Victor B Andersen <v@victorb.net>
2020-06-16 16:28:40 -0700

    Started to refactor JS for better naming

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-$(function () {
+function onDelete(event) {
+  $(e.target).closest('li').remove();
+}
+
+function waitForDelete(){ 
   // This *should* handle deletion
-  $('.shopping-list').on('click', '.shopping-item-delete', function(e) {
-    $(e.target).closest('li').remove();
-  });
-  
+  $('.shopping-list').on('click', '.shopping-item-delete', onDelete);
+}
+
+function startup() {
+  waitForDelete();  
   // This should handle toggling
-  $('.shopping-list').on('click', '.shopping-item-toggle', function (e) {
+  $('.shopping-list').on('click', '.shopping-item-toggle', function handleToggle(e) {
     $(e.target).closest('li').toggleClass('shopping-item__checked');
-      });
-  // $('.shopping-item-toggle').click(function () {
-  //   $(this).closest("li").find('.shopping-item').toggleClass('shopping-item__checked')
-  // });
-  // $('.shopping-item-delete').click(function () {
-  //   $(this).closest("li").remove()
-  // });
+  });
+
   $('#js-shopping-list-form').submit(function (event) {
     event.preventDefault()
     let item = $(this).find("#shopping-list-entry").val()
@@ -29,5 +29,7 @@ $(function () {
      </div>
    </li>`);
   });
-})
+}
+
+$(startup);
 
