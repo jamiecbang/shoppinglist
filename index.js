d6fe4491e265556a2f255dfbadef1aec13c20e93
jamiecbang <61323778+jamiecbang@users.noreply.github.com>
2020-06-17 10:42:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 $(function () {
-  // This *should* handle deletion
   $('.shopping-list').on('click', '.shopping-item-delete', function(e) {
     $(e.target).closest('li').remove();
   });
   
-  // This should handle toggling
   $('.shopping-list').on('click', '.shopping-item-toggle', function (e) {
     $(e.target).closest('li').toggleClass('shopping-item__checked');
       });
-  // $('.shopping-item-toggle').click(function () {
-  //   $(this).closest("li").find('.shopping-item').toggleClass('shopping-item__checked')
-  // });
-  // $('.shopping-item-delete').click(function () {
-  //   $(this).closest("li").remove()
-  // });
+
   $('#js-shopping-list-form').submit(function (event) {
     event.preventDefault()
     let item = $(this).find("#shopping-list-entry").val()
